030e70284d3792f782382d6e26b401e5af5fae34
wjx <626559983@qq.com>
2024-07-03 00:05:41 +0800

    1

--- op.js
+++ op.js
@@ -1,10 +1,14 @@
 const products = require("./models/products")
+const mongoose = require("mongoose")
 exports.pAdd = async (req,res)=>{
     try{
         console.log(req.body)
-        const newProducts = await products.create(req.body) 
-        console.log(newProducts)
+        const newProducts = await products.create(req.body)
+        console.log(newProducts.get("_id").toString())
+        _id  code => await dayin (_id , code)
+        
         res.json(newProducts)
+        
     }catch(error){
 
     }
