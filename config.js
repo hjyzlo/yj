21f4182a1c59a401d0422b52859c6a03929ab140
wjx <626559983@qq.com>
2024-07-03 17:49:32 +0800

    1

--- config.js
+++ config.js
@@ -2,11 +2,15 @@ exports.appPort = 80
 exports.mongoUrl = "mongodb://127.0.0.1:27018/test"
 exports.Pinfo = {
     printerName:'P1S',
-    labelX:0,
-    labelY:0,
+    qrcodeX:1,
+    qrcodeY:1,
+    textX:16,
+    textY:1,
     labelWidth:25,
     labelHeight:15,
-    margin:3,
-    qrcodeWidth:1
+    margin:1,
+    qrcodeWidth:13,
+    fontHeight:5,
+    orientation:90
 }
 module.exports = exports
--- op.js
+++ op.js
@@ -16,7 +16,8 @@ pLabel = async (_id,price)=>{
             // 2. 创建一个指定大小的标签任务
             api.startJob({width: Pinfo['labelWidth'], height: Pinfo['labelHeight']});
             // 3. 在标签纸上打印目标字符串
-            api.drawText({text:price.toString(), x:2,y:2,fontHeight:5});
+            api.drawText({text:'￥' + price, x:Pinfo['textX'],y:Pinfo['textY'],fontHeight:Pinfo['fontHeight'],orientation:Pinfo['orientation']});
+            api.draw2DQRCode({text:_id,x:Pinfo['qrcodeX'],y:Pinfo['qrcodeY'],width:Pinfo['qrcodeWidth'],orientation:Pinfo['orientation']})
             // 4. 结束绘制操作，开始打印
             api.commitJob(() => {
                 // 5. 关闭已经打开的打印机
@@ -31,7 +32,7 @@ exports.pAdd = async (req,res)=>{
         console.log(req.body)
         const newProducts = await products.create(req.body)
         console.log(newProducts.get("_id").toString())
-        await pLabel(newProducts.get("_id").toString(),newProducts.get("price"))
+        await pLabel(newProducts.get("_id").toString(),newProducts.get("price").toString())
         
         res.json(newProducts)
         
