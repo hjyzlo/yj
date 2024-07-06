d08a39e0f04882822d4e4760a5019643b5120adb
wjx <626559983@qq.com>
2024-07-06 18:26:57 +0800

    1

--- routes.js
+++ routes.js
@@ -1,9 +1,5 @@
 const express = require("express")
-<<<<<<< HEAD
-const { pAdd, login, pQuery ,upload} = require("./op")
-=======
-const { pAdd, login, pQuery,authenticateToken } = require("./op")
->>>>>>> 462c41d0e4706adb6ce923d9d72bd5d9c569f8a9
+const { pAdd, login, pQuery,authenticateToken,upload } = require("./op")
 const route = express.Router()
 
 route.get("/",(req,res)=>{
@@ -11,10 +7,6 @@ route.get("/",(req,res)=>{
 })
 route.post("/pAdd",authenticateToken,pAdd)
 route.post("/login",login)
-<<<<<<< HEAD
-route.post("/pQuery",pQuery)
 route.post('/upload',upload.single("singleFile"))
-=======
 route.post("/pQuery",authenticateToken,pQuery)
->>>>>>> 462c41d0e4706adb6ce923d9d72bd5d9c569f8a9
 module.exports = route
